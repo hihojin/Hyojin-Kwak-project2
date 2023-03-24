e5a35684486d7810bef81d3de0eaddc035f9a44f
Hyojin Kwak <hyojinkwak@Hyojins-MBP.lan>
2023-03-23 20:03:35 -0700

    removing icon library render issue

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBars} from '@fortawesome/free-solid-svg-icons';
-import { faX } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavBar() {
   const [clicked, setClicked] = useState(false);
@@ -14,24 +11,31 @@ export default function NavBar() {
 
   return (
     <nav className="navigation">
-      <h1 className={clicked ? 'hideTitle': 'navTitle'}>Wordle</h1>
+      <h1 className={clicked ? 'hideTitle' : 'navTitle'}>Wordle</h1>
       <ul className={clicked ? 'activeTable' : 'nonActiveTable'}>
-        <li className='activeFontStyle'>
+        <li className="activeFontStyle">
           <Link to="/">Home</Link>
         </li>
-        <li className='activeFontStyle'>
+        <li className="activeFontStyle">
           <Link to="/game-rule">Rule</Link>
         </li>
-        <li className='activeFontStyle'>
+        <li className="activeFontStyle">
           <Link to="/game/normal">Normal</Link>
         </li>
-        <li className='activeFontStyle'>
+        <li className="activeFontStyle">
           <Link to="/game/hard">Hard</Link>
         </li>
       </ul>
       {/* mobile menu bar when screen size goes below 500 width */}
-      {clicked ? (<FontAwesomeIcon icon={faX} className='burgerMenu' onClick={handleClick}></FontAwesomeIcon>)
-      : (<FontAwesomeIcon icon={faBars} className='burgerMenu' onClick={handleClick}></FontAwesomeIcon>)}
+      {clicked ? (
+        <div className="burgerMenu" onClick={handleClick}>
+          o
+        </div>
+      ) : (
+        <div className="burgerMenu" onClick={handleClick}>
+          x
+        </div>
+      )}
     </nav>
   );
 }
